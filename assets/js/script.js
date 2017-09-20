5761601919d384527c2076045446ce5cdedb6330
jasondreams0513 <jasondreams0513@gmail.com>
2017-09-20 02:10:07 -0500

    Added stage height resize input option for user/commented both index.html and script.js for better readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,173 @@
+/*
+Loads data from the html form in index.html into script.js
+Used for:
+  - container width
+  - container height
+ */
 function loadData(form) {
-//Background Pattern/Color changer JS portion
+
+
+
+ /*
+Background changer for stage & select drop down boxes.
+  Available Backgrounds:
+    - blueWall
+    - brick
+    - whiteBrick
+    - whiteWall
+    - wood
+    - whiteSwirl
+  */
+  $('select').change(function(){
+    if($(this).val() == 'A'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/blueWall.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+    if($(this).val() == 'B'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/brick.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+    if($(this).val() == 'C'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteBrick.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+    if($(this).val() == 'D'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteWall.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+    if($(this).val() == 'E'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/wood.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+    if($(this).val() == 'F'){
+      $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteSwirl.jpg)');
+      $("select, #container, #add-shape").css('background-size', 'cover');
+      $("select, #container, #add-shape").css('color', 'transparent');
+      $("select, #container, #add-shape").css('font-style', 'bold');
+
+    }
+  });
+
+
+
+/*
+Frame Changer for select drop down box
+  Available Frame Sizes:
+    - 4 x 6
+    - 5 x 7
+    - 6 x 8
+    - 10 x 13
+    - 11 x 14
+    - 16 x 20
+    - 18 x 24
+    - 20 x 24
+    - 24 x 30
+    - 24 x 36
+
+Warning: To select the same frame size more than once, you must choose the
+blank option to reset the choice. Once that is done you can then select the
+frame size again.
+ */
 $('select').change(function(){
-if($(this).val() == 'A'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/blueWall.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'cover');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
 
-}
-if($(this).val() == 'B'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/brick.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'cover');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
+  if($(this).val() == 'add-shape'){
+    addShape();
+  }
 
-}
-if($(this).val() == 'C'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteBrick.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'cover');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
-
-}
-if($(this).val() == 'D'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteWall.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'cover');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
-
-}
-if($(this).val() == 'E'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/wood.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'cover');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
-
-}
-if($(this).val() == 'F'){
-  $("select, #container, #add-shape").css('background-image', 'url(assets/css/img/whiteSwirl.jpg)');
-  $("select, #container, #add-shape").css('background-size', 'form.wallWidth.value');
-  $("select, #container, #add-shape").css('color', 'transparent');
-  $("select, #container, #add-shape").css('font-style', 'bold');
-
-}
+  if($(this).val() == 'another-shape'){
+    anotherShape();
+  }
 
 });
 
 
-//Frame Changes
-$('select').change(function(){
-if($(this).val() == 'add-shape'){
-  addShape();
 
-}
-if($(this).val() == 'another-shape'){
-  anotherShape();
+/*
+1. Grabbing the width of the #container based on input (form.wallWidth.value)
+from the html form in index.html.
+Cannot put them in a function due to the need to have them global so that
+they can be pulled into the Konva.Stage below.
 
-}
-});
+2.  Grabbing the height of the #container based on input (form.wallHeight.value)
+from the html form in index.html.
+Cannot put them in a function due to the need to have them global so that
+they can be pulled into the Konva.Stage below
 
+3. Creating a variable 'control' that acts as a holder for my buttons and
+drop down boxes. Below I add it to a layer so the background doesnt cover it
+and to do that I have to call it from index.html into script.js
+ through --> document.getElementById('controlPanel').
+ */
+  var width = form.wallWidth.value;
+  var height = form.wallHeight.value;
 
-  var test = form.wallWidth.value;
-  console.log(test);
+  var container = document.getElementById('container');
+  console.log(container);
 
-  var ugh = document.getElementById('container');
-  console.log(ugh);
-  ugh.style.width = test + 'px';
-  console.log(ugh.style.width);
+  container.style.width = width + 'px';
+  console.log(container.style.width);
+  container.style.height = height + 'px';
 
   var control = document.getElementById('controlPanel');
-  console.log(control);
-//Creating Stage
+
+
+
+/*
+1.Created a variable named 'stage' that creates a new Konva.Stage, notice here
+the container is referencing the html div id #container in index.html.
+
+2. The 'width: width' is referencing up above.
+
+3. Will add 'heigh: height' is referencing up above
+ */
   var stage = new Konva.Stage({
 
     container: "container",
-    width: test,
-    height: 480,
+    width: width,
+    height: height,
 });
 
-//Original Size
+
+
+/*
+Adding a click function for when the user clicks on the drop down box and
+selects #add-shape then the function addShape() will be triggered
+ */
 jQuery("#add-shape").click(function() {
   addShape();
 });
 
+
+
+/*
+Function addShape(): Encompasses a function that on click will create a frame
+(w/ var parentContainer & var circle) on the stage.
+
+Function cirlce.on(): Acts as the marker for the frame (parentContainer). On
+click it triggers the tooltip to be printed on the frame.
+
+Var parentContainer creates a new Konva.Rect that acts as the frame
+
+Var circle creates a new Konva.Circle that I attached to the the parentContainer
+to act as a nail and a marker.
+
+Var tooltip creates a text layer that I use to display the tooltip
+ */
 var addShape = function() {
   console.log("add shape");
   var layer = new Konva.Layer();
@@ -94,8 +179,8 @@ var addShape = function() {
   var tooltipLayer = new Konva.Layer();
 
   var parentContainer = new Konva.Rect({
-    x: 50,
-    y: 50,
+    x: 200,
+    y: 200,
     width: 76.8,
     height: 96,
     fill: '#65666c',
@@ -157,11 +242,31 @@ var addShape = function() {
 }
 
 
-//NEW Size
+
+/*
+Adding a click function for when the user clicks on the drop down box and
+selects #another-shape then the function anotherShape() will be triggered
+ */
 jQuery("#another-shape").click(function() {
   anotherShape();
 });
 
+
+
+/*
+Function anotherShape(): Encompasses a function that on click will create a frame
+(w/ var parentContainer & var circle) on the stage.
+
+Function cirlce.on(): Acts as the marker for the frame (parentContainer). On
+click it triggers the tooltip to be printed on the frame.
+
+Var parentContainer creates a new Konva.Rect that acts as the frame
+
+Var circle creates a new Konva.Circle that I attached to the the parentContainer
+to act as a nail and a marker.
+
+Var tooltip creates a text layer that I use to display the tooltip
+ */
 var anotherShape = function() {
   console.log("another shape");
   var layer = new Konva.Layer();
@@ -173,8 +278,8 @@ var anotherShape = function() {
   var tooltipLayer = new Konva.Layer();
 
   var parentContainer = new Konva.Rect({
-    x: 50,
-    y: 50,
+    x: 200,
+    y: 200,
     width: 48,
     height: 67.2,
     fill: '#65666c',
